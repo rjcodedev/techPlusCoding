434b1fb92ce1fd1b59bad77e3bf4901a4c36a82e
coderguysishere <gauravshegokar2312@gmail.com>
2023-01-15 21:40:53 +0530

    added contact page

--- src/component/Contact.js
+++ src/component/Contact.js
@@ -1,9 +1,10 @@
 import React from 'react'
 
 const Contact = () => {
-  return (
-    <div>Contact</div>
-  )
+    return (
+        <div><h1>Contact</h1>
+            <p>Email id:<a href="mailto:contactlearnwebdevelopment@gmail.com">contactlearnwebdevelopment@gmail.com</a> </p></div>
+    )
 }
 
 export default Contact
--- src/component/Header.js
+++ src/component/Header.js
@@ -1,19 +1,19 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
 import headLogo from '../assets/head-logo.png'
 // import cartLogo from '../assets/cart-logo.png'
 
 const Header = () => {
     return (
         <div className='bg-[#b3ecff] flex justify-between py-1 px-2' >
-            <div><img src={headLogo} alt='img'className='md:w-10 md:w-10 w-10 h-10'/></div>
+            <Link to='/'><img src={headLogo} alt='img' className='md:w-10 md:h-10 w-10 h-10' /></Link>
 
             <div className='flex justify-between items-center'>
-                <div className='pr-3 font-bold'>About</div>
-                <div className='pr-3 font-bold'>Contact</div>
+                {/* <Link to='about' className='pr-3 font-bold'>About</Link> */}
+                <Link to='contact' className='pr-3 font-bold'>Contact</Link>
                 {/* <div className='pr-3 font-bold'><img src={cartLogo} alt='img'className='md:w-10 md:w-10 w-10 h-10'/></div> */}
             </div>
         </div>
     )
 }
-
 export default Header
